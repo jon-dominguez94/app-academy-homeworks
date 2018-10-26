a4a8c9beb2ae363ce74cbd3ad947f20b82e782a3
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-10-25 21:27:44 -0700

    Add #push, #pop, and #peek methods for Stack

--- W1D5/adt.rb
+++ W1D5/adt.rb
@@ -1,6 +1,20 @@
 class Stack
+  attr_accessor :stack
+  
   def initialize
     @stack = []
+  end
+
+  def push(el)
+    stack.push(el)
+  end
+
+  def pop
+    stack.pop
+  end
+
+  def peek
+    stack.last
   end
 end
 
