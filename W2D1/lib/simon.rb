15dd61297545a2d6f73ac3a4542d9fdfa3a6c2c6
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-10-28 20:21:46 -0700

    Implement #require_sequence method

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -33,10 +33,18 @@ class Simon
       sleep(1)
       puts %x{clear}
       sleep(0.25)
+    end
   end
 
   def require_sequence
-
+    puts "Repeat the sequence by typing the first letter or each color then pressing enter. (r,b,g,y)"
+    @seq.each do |color|
+      ans = gets.chomp
+      unless ans == color[0]
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
