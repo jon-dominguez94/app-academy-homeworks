fd33e151ad7a43e92bfcd96066adcb24353c2c2c
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-10-29 22:24:02 -0700

    Implement #initialize and #place_stones method

--- w2d2/skeleton/lib/board.rb
+++ w2d2/skeleton/lib/board.rb
@@ -2,13 +2,25 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14){Array.new}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    cups.each_with_index do |cup, i|
+      next if i == 6 || i == 13
+      4.times do
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" unless start_pos.between?(0, cups.length)
+    raise "Starting cup is empty" if cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
@@ -26,9 +38,5 @@ class Board
     puts ""
   end
 
-  def one_side_empty?
-  end
-
-  def winner
-  end
+  
 end
