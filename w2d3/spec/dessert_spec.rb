e5396c37a4032dbffc81ab25f50eede67867f481
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-10-30 20:59:16 -0700

    Write specs for #initialize

--- w2d3/spec/dessert_spec.rb
+++ w2d3/spec/dessert_spec.rb
@@ -7,19 +7,29 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
+  subject(:brownie) { Dessert.new('brownie', 20, chef)}
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(brownie.type).to eq('brownie')
+    end
 
-    it "sets a quantity"
+    it "sets a quantity"do
+      expect(brownie.quantity).to eq(20)
+    end
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
 
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new('pie', 'twenty', chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      
+    end
   end
 
   describe "#mix!" do
