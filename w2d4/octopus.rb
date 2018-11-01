c4ab8b2701d8d4488706c3d83efbeda91548bccc
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-01 07:25:03 -0700

    Write sluggish search method O(n^2)

--- w2d4/octopus.rb
+++ w2d4/octopus.rb
@@ -1,1 +1,9 @@
-
+def sluggish(array)
+  longest_fish = ""
+  array.each do |fish|
+    array.each do |other_fish|
+      longest_fish = fish if fish.length > other_fish.length
+    end
+  end
+  longest_fish
+end
