d2fc177752c09954aa33e7db4bc96e061f61fe25
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-01 07:26:16 -0700

    Write clever search method O(n)

--- w2d4/octopus.rb
+++ w2d4/octopus.rb
@@ -7,3 +7,44 @@ def sluggish(array)
   end
   longest_fish
 end
+
+def dominant(array)
+  merge_sort(array).last
+end
+
+def merge_sort(array)
+  return array if array.length <= 1
+  mid = array.length / 2
+
+  left = array[0...mid]
+  right = array[mid..-1]
+
+  merge(merge_sort(left), merge_sort(right))
+end
+
+def merge(left, right)
+  return right if left.empty?
+  return left if right.empty?
+
+  result = []
+
+  until left.empty? || right.empty?
+    if left.first.length < right.first.length
+      result << left.shift
+    else
+      result << right.shift
+    end
+  end
+
+  result + left + right
+end
+
+def clever(array)
+  longest = array[0]
+
+  array[1..-1].each do |el|
+    longest = el if el.length > longest.length
+  end
+
+  longest
+end
