fe1ddb5e3f019e7003f9ac9f7dcb2b5b3a219518
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-01 21:04:35 -0700

    Write spec for #initialize to create @cache instance variable

--- w2d5/spec/lru_spec.rb
+++ w2d5/spec/lru_spec.rb
@@ -6,5 +6,8 @@ RSpec.describe LRUCache do
     it 'exposes the size instance variable' do
       expect(cache.size).to eq(4)
     end
+    it 'creates an empty array @cache instance variable' do
+      expect(cache.cache).to be_empty
+    end
   end
 end
