9b956292e441d868eb8419ee16c0751347facfd7
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-01 21:09:59 -0700

    Write first spec for #add

--- w2d5/spec/lru_spec.rb
+++ w2d5/spec/lru_spec.rb
@@ -17,6 +17,12 @@ RSpec.describe LRUCache do
     it 'adds a new element to the cache' do
       expect(cache.cache).to eq([1])
     end
+    it 'adds many elements to the cache' do
+      cache.add("hi")
+      cache.add({a:2, b:"hello"})
+      cache.add([1,2,3])
+      expect(cache.cache).to eq([1], "hi", {a:2, b:"hello"},[1,2,3])
+    end
   end
 
   describe '#count' do
