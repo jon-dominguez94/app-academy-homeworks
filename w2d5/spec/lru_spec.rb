9916f341f184833782065915181556e8562ae8fb
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-01 21:08:07 -0700

    Write first spec for #add

--- w2d5/spec/lru_spec.rb
+++ w2d5/spec/lru_spec.rb
@@ -1,13 +1,25 @@
 require 'lru'
 
 RSpec.describe LRUCache do
+  subject(:cache) { LRUCache.new(4)}
+
   describe '#initialize' do
-    subject(:cache) { LRUCache.new(4)}
     it 'exposes the size instance variable' do
       expect(cache.size).to eq(4)
     end
     it 'creates an empty array @cache instance variable' do
       expect(cache.cache).to be_empty
     end
+  end
+
+  describe '#add' do
+    before(:each) { cache.add(1) }
+    it 'adds a new element to the cache' do
+      expect(cache.cache).to eq([1])
+    end
+  end
+
+  describe '#count' do
+
   end
 end
