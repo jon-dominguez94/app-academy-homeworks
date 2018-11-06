bfeb87924118ffde7dfb2dea2015238d7e16828b
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-06 07:16:01 -0800

    Write #update method

--- w3d2/plays.rb
+++ w3d2/plays.rb
@@ -122,4 +122,17 @@ class Playwright
     SQL
     self.id = PlayDBConnection.instance.last_insert_row_id
   end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
 end
