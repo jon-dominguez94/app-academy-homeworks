aa1a0a69747068786aaa1ab2678c3e80acc42633
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-06 07:17:23 -0800

    Write #get_plays method

--- w3d2/plays.rb
+++ w3d2/plays.rb
@@ -135,4 +135,17 @@ class Playwright
     SQL
   end
 
+  def get_plays
+    raise "#{self} not in database" unless self.id
+    plays = PlayDBConnection.instance.execute(<<-SQL, self.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    plays.map { |play| Play.new(play) }
+  end
+
 end
