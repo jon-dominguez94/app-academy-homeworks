275de69cae23ca18d58ec8b09ec6174013f41cb8
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-07 06:17:45 -0800

    Write seed file

--- w3d3/demo_project/db/seeds.rb
+++ w3d3/demo_project/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+House.create(address: "Stark")
+House.create(address: "Tyrell")
+House.create(address: "Lannister")
+House.create(address: "Targaryen")
+
+Person.create(name: "Jon", house_id: 1)
+Person.create(name: "Ned", house_id: 1)
+Person.create(name: "Margery", house_id: 2)
+Person.create(name: "Tyrion", house_id: 3)
+Person.create(name: "Jamie", house_id: 3)
+Person.create(name: "Danerys", house_id: 4)
