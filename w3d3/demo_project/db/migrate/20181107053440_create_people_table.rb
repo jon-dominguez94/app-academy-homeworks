31217d457565b99267b76bf57037b25bd5d47858
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-06 21:46:36 -0800

    Create Houses migration table

--- w3d3/demo_project/db/migrate/20181107053440_create_people_table.rb
+++ w3d3/demo_project/db/migrate/20181107053440_create_people_table.rb
@@ -1,6 +1,9 @@
 class CreatePeopleTable < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
+      t.string :name, null: false
+      t.integer :house_id, null: false
+      t.timestamps
     end
   end
 end
---
+++ w3d3/demo_project/db/migrate/20181107054601_create_houses.rb
@@ -0,0 +1,6 @@
+class CreateHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :houses do |t|
+    end
+  end
+end
