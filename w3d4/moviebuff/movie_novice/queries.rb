01e289c407c2ce0694a968e9b2b28151d036eee8
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-07 20:48:31 -0800

    Pass pulp_fiction_movies spec

--- w3d4/moviebuff/movie_novice/queries.rb
+++ w3d4/moviebuff/movie_novice/queries.rb
@@ -70,7 +70,9 @@ def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor
+  .joins(:movies)
+  .where('title = ?', 'Pulp Fiction')
 end
 
 def uma_movies
