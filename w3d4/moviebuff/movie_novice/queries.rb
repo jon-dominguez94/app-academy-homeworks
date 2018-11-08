2f55274411b769a4900806a4294e5846ac4f51b9
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-07 20:37:13 -0800

    Finish star_wars spec

--- w3d4/moviebuff/movie_novice/queries.rb
+++ w3d4/moviebuff/movie_novice/queries.rb
@@ -36,6 +36,7 @@ end
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
+  Movie.select(:id, :title, :yr).where('title like ?', 'Star Wars%')
 
 end
 
