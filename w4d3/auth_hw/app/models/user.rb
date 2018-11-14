49a60ef1ac4506b2b44be9130f9509ded9b2101b
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-14 07:03:58 -0800

    Create password_digest validation for user

--- w4d3/auth_hw/app/models/user.rb
+++ w4d3/auth_hw/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :session_token, presence: true, uniqueness: true
-  
+  validates :password_digest, presence: {message: 'Password can\'t be blank'}
 
 end
