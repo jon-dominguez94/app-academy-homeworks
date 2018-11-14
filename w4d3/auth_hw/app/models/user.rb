c8568342893d43e454b021ef1ba814df51a7599b
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-14 07:07:52 -0800

    Ensure session_token before validation

--- w4d3/auth_hw/app/models/user.rb
+++ w4d3/auth_hw/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
-
+  before_validation :ensure_session_token
 end
