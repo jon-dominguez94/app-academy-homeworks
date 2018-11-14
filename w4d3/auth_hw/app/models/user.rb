9f6d47efbab4b222bf6e4a97d50a1a3043c14469
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-14 07:10:35 -0800

    Write User::find_by_credentials

--- w4d3/auth_hw/app/models/user.rb
+++ w4d3/auth_hw/app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
 end
