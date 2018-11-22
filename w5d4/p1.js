ab4a81ccc149a254b3da7314e5c833ae3b2ad54f
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-22 05:49:49 -0800

    Implement fizzbuzz

--- w5d4/p1.js
+++ w5d4/p1.js
@@ -1,14 +1,18 @@
 function fizzbuzz(arr) {
-  fizzes = [];
-  for(let i = 0; i < arr.length, i++) {
-    if ((arr[i] % 3) == 0) {
-      if((arr[i] % 5) != 0) {
-        fizzes << arr[i]
+  const fizzes = [];
+  for(let i = 0; i < arr.length; i++) {
+    if ((arr[i] % 3) === 0) {
+      if((arr[i] % 5) !== 0) {
+        fizzes.push(arr[i]);
       }
+    }
     else {
-      if((arr[i]) % 5) {
-        fizzes << arr[i]
+      if((arr[i]) % 5 === 0) {
+        fizzes.push(arr[i]);
       }
     }
   }
+  return fizzes;
 }
+
+console.log(fizzbuzz([1,2,3,4,5,15,24,30,33]));//.forEach()
