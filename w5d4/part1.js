77f7be2f6ac67bbd37ba19f1420c84093dd1f366
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-21 22:49:48 -0800

    Comment out method5 to prevent errors

--- w5d4/part1.js
+++ w5d4/part1.js
@@ -48,8 +48,15 @@ function mysteryScoping5() {
     let x = 'in block';
     console.log(x);
   }
-  let x = 'out of block again';
+  // let x = 'out of block again';
   console.log(x);
 }
 
 mysteryScoping5();
+
+
+function madLib(verb, adj, noun) {
+  return `We shall ${verb} the ${adj} ${noun}`
+}
+
+console.log(madLib('run', 'quickly', 'cars'));
