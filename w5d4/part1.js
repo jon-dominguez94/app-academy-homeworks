09bd5da239ed20a40bbe4fd475b8deaa92881f30
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-21 22:45:14 -0800

    Write mysteryScoping3

--- w5d4/part1.js
+++ w5d4/part1.js
@@ -19,3 +19,14 @@ function mysteryScoping2() {
 }
 
 mysteryScoping2();
+
+function mysteryScoping3() {
+  const x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping3();
