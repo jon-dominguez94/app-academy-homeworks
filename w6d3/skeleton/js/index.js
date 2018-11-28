a428afea67d3ead27af206cf9ed3846456d1f197
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-28 08:25:22 -0800

    Add success and error callbacks

--- w6d3/skeleton/js/index.js
+++ w6d3/skeleton/js/index.js
@@ -5,8 +5,14 @@ console.log("Hello from the JavaScript console!");
 $.ajax({
   type: 'GET',
   url: 'http://api.openweathermap.org/data/2.5/weather' +
-    '?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b'
-  
+    '?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(data) {
+    console.log("Here's the weather!")
+    console.log(data);
+  },
+  error() {
+    console.error("Error getting weather.");
+  },
 });
 
 // Add another console log here, outside your AJAX request
