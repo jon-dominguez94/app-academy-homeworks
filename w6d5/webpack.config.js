1838338a7f773da44789432264ffd99ba020c698
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-30 08:16:02 -0800

    Configure babel transpilation

--- w6d5/webpack.config.js
+++ w6d5/webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assest', 'javascripts'),
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
