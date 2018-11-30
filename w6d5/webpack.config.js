dd18df2d75f46afff58235400fd168d7098f2c1a
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-30 08:10:52 -0800

    Setup entry and output in webpack

---
+++ w6d5/webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./frontend/index.js",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assest', 'javascripts'),
+    filename: "bundle.js"
+  }
+};
